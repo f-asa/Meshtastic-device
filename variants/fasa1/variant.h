c9568b3e6187a28197bcb80a642bf1e3b324ee66
f-asa <34549757+f-asa@users.noreply.github.com>
2021-02-19 18:08:58 -0500

    Create variant.h

---
+++ variants/fasa1/variant.h
@@ -0,0 +1,12 @@
+#ifndef _VARIANT_FASA_V1_
+#define _VARIANT_FASA_V1_
+
+#elif defined(FASA_V1)
+#define HW_VENDOR "fasa1"
+
+#define REQUIRE_RADIO false
+
+#define SSD1306_ADDRESS 0x3C
+
+#define I2C_SDA 21
+#define I2C_SCL 22
